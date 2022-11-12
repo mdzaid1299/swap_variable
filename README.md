a61e9591fb656d19aa0894cc785abf71d6de9dfa
HP <mdzaid1299@gmail.com>
2022-11-12 21:33:47 +0530

    ini

---
+++ README.md
@@ -0,0 +1,10 @@
+3. Swap without third variable
+
+a = 10
+b = 20
+
+o/p : a = 20,
+b= 10.
+step 1 : take both a and b value from user
+step 2 : use logic
+(arithmetic operator)
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a, b, temp;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter value of a  & b");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println("before swapping =  a +" + a + " b = " + b);
+        a =b +a;
+        b = a -b;
+        a = a - b;
+        System.out.println("after swap = " + a + " b = " + b);
     }
 }
